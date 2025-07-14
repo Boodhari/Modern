e514328bbac3c3dbb64eeb8ab838c9dbe00e6af9
Abadir Hassan <34312697+Boodhari@users.noreply.github.com>
2025-07-14 14:11:24 +0300

    connection

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("sql8.freesqldatabase.com", "sql12789914", "QyEQRyamui", "sql12789914");
+$conn = new mysqli("sql12.freesqldatabase.com", "sql12789914", "QyEQRyamui", "sql12789914");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }

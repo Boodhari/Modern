1b9f2848030e462d7a049fd4c4773dd55956174b
Abadir Hassan <34312697+Boodhari@users.noreply.github.com>
2025-07-15 12:58:20 +0300

    make footer container fluid

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 
 
 <footer class="bg-primary text-white text-center py-3 mt-5">
-  <div class="container">
+  <div class="container-fluid">
     <p class="mb-0">&copy; <?= date('Y') ?> Magan Tech. All rights reserved.</p>
   </div>
 </footer>
